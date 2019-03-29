31e2188b8d7111a60f46a5c1804daa97c0f7feea
liujunyang <liujunyangscript@gmail.com>
2019-03-29 19:16:17 +0800

    add socket server

---
+++ server.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# server
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('localhost', 8080))
+sock.listen(1)
+while True:
+    conn, addr = sock.accept()
+    print conn.recv(1024)
+    conn.sendall('world')
+    conn.close()
+
