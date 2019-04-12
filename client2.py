39bf19b80e2b93ca6b825dca7dfd6e3cbd636cbf
liujunyang <liujunyangscript@gmail.com>
2019-04-12 19:12:38 +0800

    add blocking_single.py

---
+++ client2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# client
+
+import socket
+import struct
+import json
+import time
+
+def rpc(sock, in_, params):
+    request = json.dumps({"in": in_, "params": params})
+    length_prefix = struct.pack("I", len(request))
+    sock.sendall(length_prefix)
+    sock.sendall(request)
+    length_prefix = sock.recv(4)
+    length, = struct.unpack("I", length_prefix)
+    body = sock.recv(length)
+    response = json.loads(body)
+    return response["out"], response["result"]
+
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("localhost", 8080))
+    for i in range(10):
+        out, result = rpc(s, "ping", "ireader %d" % i)
+        print out, result
+        time.sleep(1)
+    s.close()
