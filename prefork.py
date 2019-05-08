1da90fa59319a0159de544fc4766b4bdfa771df3
liujunyang <liujunyangscript@gmail.com>
2019-05-08 11:34:47 +0800

    add prefork.py

--- prefork.py
+++ prefork.py
@@ -4,7 +4,6 @@ import os
 import struct
 import json
 import socket
-import multiprocessing
 
 def handle_conn(conn, addr, handlers):
     print addr, "comes"
@@ -14,7 +13,7 @@ def handle_conn(conn, addr, handlers):
             print addr, "bye"
             conn.close()
             break
-        length, = struct.unpack("I", length_prefix) # 将一个 4 字节的字符串解码成一个整数
+        length, = struct.unpack("I", length_prefix)
         body = conn.recv(length)
         request = json.loads(body)
         in_ = request["in"]
@@ -26,15 +25,7 @@ def handle_conn(conn, addr, handlers):
 def loop(sock, handlers):
     while True:
         conn, addr = sock.accept()
-        pid = os.fork() # 创建子进程处理新连接
-        if pid < 0:
-            return
-        if pid > 0:
-            conn.close()
-        if pid == 0:
-            sock.close()
-            handle_conn(conn, addr, handlers)
-            break
+        handle_conn(conn, addr, handlers)
 
 def ping(conn, params):
     send_result(conn, "pong", params)
@@ -45,11 +36,22 @@ def send_result(conn, out, result):
     conn.sendall(length_prefix)
     conn.sendall(response)
 
+def prefork(n):
+    for i in range(n):
+        pid = os.fork()
+        if pid < 0:
+            return
+        if pid > 0:
+            continue
+        if pid == 0:
+            break
+
 if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(('localhost', 8080))
     sock.listen(1)
+    prefork(10)
     handlers = {
         "ping": ping
     }
